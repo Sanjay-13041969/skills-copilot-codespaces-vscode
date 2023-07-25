bda16c1a3502b08d3f80694d87e351553339d859
Sanjay-13041969 <139948368+Sanjay-13041969@users.noreply.github.com>
2023-07-25 05:11:33 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,41 @@
+function skillsMember() {
+    var skills = [
+        {
+            name: "HTML",
+            level: "90%"
+        },
+        {
+            name: "CSS",
+            level: "80%"
+        },
+        {
+            name: "Javascript",
+            level: "70%"
+        },
+        {
+            name: "PHP",
+            level: "60%"
+        },
+        {
+            name: "C#",
+            level: "50%"
+        },
+        {
+            name: "Java",
+            level: "40%"
+        },
+        {
+            name: "Python",
+            level: "30%"
+        },
+        {
+            name: "Ruby",
+            level: "20%"
+        },
+        {
+            name: "C++",
+            level: "10%"
+        }
+    ];
+    return skills;
+}
